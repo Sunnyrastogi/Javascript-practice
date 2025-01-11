0caa4624bf21b413862aa890f8d717b66c2f9856
Sunny Rastogi <90035466+Sunnyrastogi@users.noreply.github.com>
2025-01-11 21:19:37 +0530

    js learning

---
+++ javascript.js
@@ -0,0 +1,146 @@
+// console.log("Sunny")
+
+// const accountId= 5644646
+// let emailId = "sunnyrastogi22@gmail.com"
+// var accountPassword = "484866"
+// let serviceId;
+
+
+// console.table([serviceId,accountPassword,emailId,accountId])
+// let age = 42;
+
+// if(age>18){
+//     console.log("YOu can vote")
+// }
+// else{
+//     console.log("You can't vote")
+// }
+ 
+// accountPassword= 65465464
+
+// console.log(accountPassword);
+// const array=[1,2,3,4,5]
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i])
+// }
+
+// accountId = 475458
+
+// console.log(accountId)
+
+// emailId = "test@gmail.com"
+// console.log(emailId);
+
+// age=52
+// console.log(age)
+
+
+// alert(3+3)
+
+
+// Data Types in JAVASCRIPT
+
+// let number=54545      
+
+// let name ="sunny"
+
+// let isLoggedIn = true
+
+// let temperature= null;
+
+// let sigma;
+
+// console.log(typeof(temperature))
+
+// let score= null
+
+// let valueInNo = Number(score)
+// console.log(typeof valueInNo)
+// console.log(valueInNo)
+
+// let win =true;
+
+// let prob = number(win)
+
+// console.log("prob",prob);
+
+// 
+
+// let name ="Sunny"
+
+// let car ="mustang"
+
+// // console.log(name +" "+car)
+
+// console.log(`Hi this is ${name} and I have a ${car}`);
+
+// let gameName = new String('Sunny-rastogi-hc')
+
+// // console.log(gameName.toLowerCase())
+
+// console.log(gameName.charAt(2))
+
+// console.log(gameName.includes("n"))
+//  console.log(gameName.indexOf('y'))
+
+// console.log(gameName.length)
+
+// console.log(gameName.substring(0,4))
+
+// console.log(gameName.slice(-5,3))
+
+// console.log(gameName.split("-"))
+
+
+// const anotherString = "   hdffuvhu  "
+
+// console.log(anotherString.trim())
+
+// const url ="https//sunny.com%20universe"
+
+
+// console.log(url.replace('%20','-')); 
+
+// window.alert(3+3)
+
+// let number =5  // number
+// let string="sunny"  // string
+// let loggedIn =true // boolean 
+
+// let age;  // undefined
+// let temperature =null;  // standalone value 
+
+// // console.table([typeof number,string,loggedIn,age,temperature])
+
+
+
+// console.log(typeof null)
+
+
+
+// const gameName = String('Sunny')
+
+// console.log(gameName.charAt(2))
+
+
+// console.log(gameName.indexOf('n'))
+
+// console.log(gameName.substring(0,5))
+
+// console.log(gameName.slice(0,4))
+
+// console.log(gameName.includes("nn"))
+
+
+const url="Devraj-Frontend-Developer"
+const newUrl = url.split("-")
+newUrl.pop()
+console.log(newUrl)
+
+
+
+// const array =[1,2,3,4,5]
+
+// console.log(array.pop())
+
+
